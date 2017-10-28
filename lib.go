4709bfca81c93cda8746f493364278563eb2d519
Liam Naddell <liamnprg@gmail.com>
2017-10-28 12:02:34 -0400

    fix errs, remove some comments, integer validation

--- lib.go
+++ lib.go
@@ -36,109 +36,13 @@ type Menuitem struct {
 	DataName   string
 }
 
-/*
-func testusage() {
-	var ri = Rootitem{
-		Data:      make(map[string]string),
-		NextTitle: "Which submenu do u want",
-		Lower: []Menuitem{
-			{
-				Prompt: "Pick me from the menu using numbers",
-				Oncomplete: func(d *Data) {
-					fmt.Println("Just cuz you pick numbers, does not mean you know them -Albert Winestien")
-				},
-				NextTitle: "How many numbers do you want?",
-				Lower: []Menuitem{
-					{
-						Prompt: "I use lots of numbers",
-					},
-					{
-						Prompt: "I use more numbers",
-					},
-					{
-						Prompt: "I use a heck of a lot more numbers, pick me!",
-					},
-				},
-			},
-			{
-				Prompt:    "NO PICK ME PLEASE I HAVE COOL SUBMENUS",
-				NextTitle: "What do you want to be cool with?",
-				Lower: []Menuitem{
-					{
-						Prompt: "Do you want to be cool with sunglasses",
-					},
-					{
-						Prompt: "Do you want to be cool with shadez",
-					},
-					{
-						Prompt: "Do you want to be cool with ice",
-					},
-				},
-				Oncomplete: func(d *Data) {
-					fmt.Println("You will never be cool lol!")
-				},
-			},
-			{
-				Prompt:    "Click here for free youtube red subscriptions",
-				NextTitle: "How long do you want it to last for?",
-				Lower: []Menuitem{
-					{
-						Prompt: "Do you want it to last for eight months",
-					},
-					{
-						Prompt: "Do you want it to last for four months",
-					},
-					{
-						Prompt: "Do you want it to last for one month",
-					},
-				},
-				Oncomplete: func(d *Data) {
-					fmt.Println("You now have a virus on your pc lol")
-				},
-			},
-			{
-				Prompt: "Entering Data test",
-				Lower: []Menuitem{
-					{
-						Prompt:   "Please enter the number 42",
-						Type:     "UserData",
-						DataName: "numberone",
-						Oncomplete: func(Data *Data) {
-							if (*Data)["numberone"] == "42" {
-								fmt.Println("Thank you good sir")
-							} else {
-								fmt.Println("Boo you suck")
-							}
-						},
-					},
-					{
-						Prompt:   "Please enter the number 12",
-						Type:     "UserData",
-						DataName: "numbertwo",
-						Oncomplete: func(Data *Data) {
-							if (*Data)["numbertwo"] == "12" {
-								fmt.Println("Good job, you can see and press 12")
-							} else {
-								fmt.Println("Really, do better man")
-							}
-						},
-					},
-				},
-			},
-		},
-	}
-	litter.Dump(ri)
-	fmt.Println("")
-	RootTrav(ri)
-}
-*/
-func RootTrav(r Rootitem) {
+func RootTrav(r Rootitem) error {
 	fmt.Println(r.NextTitle)
-	Menutrav(r.Lower, &r.Data, r.Skippable)
+	return Menutrav(r.Lower, &r.Data, r.Skippable)
 }
-func Menutrav(ra []Menuitem, Data *Data, skippable bool) {
+func Menutrav(ra []Menuitem, Data *Data, skippable bool) error {
 	if exit {
-		return
+		return nil
 	}
 	var userDataprompt bool
 	for i := 0; i < len(ra); i++ {
@@ -153,7 +57,7 @@ func Menutrav(ra []Menuitem, Data *Data, skippable bool) {
 				for i := 0; i < len(exits); i++ {
 					if userdata == exits[i] {
 						exit = true
-						return
+						return nil
 					}
 				}
 			}
@@ -165,7 +69,7 @@ func Menutrav(ra []Menuitem, Data *Data, skippable bool) {
 		}
 	}
 	if userDataprompt == true {
-		return
+		return nil
 	}
 	var resi = -1
 	for {
@@ -174,11 +78,26 @@ func Menutrav(ra []Menuitem, Data *Data, skippable bool) {
 			for i := 0; i < len(exits); i++ {
 				if res == exits[i] {
 					exit = true
-					return
+					return nil
 				}
 			}
 		}
-		resi = toint(res)
+		var c bool
+		for {
+			var err error
+			resi, err = toint(res)
+			if err != nil {
+				fmt.Println("Please input a valid integer!")
+				c = true
+				break
+			} else {
+				c = false
+				break
+			}
+		}
+		if c {
+			continue
+		}
 		if resi > -1 {
 			if resi < len(ra) {
 				break
@@ -188,32 +107,26 @@ func Menutrav(ra []Menuitem, Data *Data, skippable bool) {
 	}
 	if ra[resi].Lower != nil {
 		fmt.Println(ra[resi].NextTitle)
-		Menutrav(ra[resi].Lower, Data, skippable)
+		err := Menutrav(ra[resi].Lower, Data, skippable)
+		if err != nil {
+			return err
+		}
 	}
 	if ra[resi].Oncomplete != nil && !exit {
 		ra[resi].Oncomplete(Data)
 	}
-
+	return nil
 }
-func toint(s string) int {
+func toint(s string) (int, error) {
 	i, err := strconv.Atoi(s)
 	if err != nil {
-		panic(err)
+		return 0, err
 	}
-	return i
+	return i, nil
 }
 func input(str string) string {
 	fmt.Print("\n" + str)
 	r := bufio.NewScanner(os.Stdin)
 	r.Scan()
 	return r.Text()
-
 }
-
-/*func menutrav(ra []menuitem) {
-	for i := 0; i < len(ra); i++ {
-		fmt.Println(ra[i].Name)
-		menutrav(ra[i].Lower)
-	}
-}
-*/
